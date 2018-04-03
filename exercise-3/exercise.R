d91f9244b7074cf56e5beb2af9bd160ec9bd944c
rosejao <rosejao@uw.edu>
2018-04-02 17:07:54 -0700

    in-class exercise ch6

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -15,4 +15,10 @@
 
 # Create a variable `height_in_meters` by passing your height in imperial to the
 # `imperial_to_metric` function
+imperial_to_metric <- function(feet, inches) {
+  total_inches <- feet*12 + inches
+  meters <- total_inches * 0.0254
+  meters # return the value in meters
+}
 
+height_in_meters <- imperial_to_metric(5,11)
